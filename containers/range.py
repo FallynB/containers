2b645f2b2c452c2e3d7100bbf4954b13e4f2b953
Fallyn Buckner <Fallyn.Buckner.26@cmc.edu>
2023-04-02 07:57:25 -0700

    whitespace fix 3

--- containers/range.py
+++ containers/range.py
@@ -1,14 +1,14 @@
 def range(a, b=None, c=None):
     if b is not None:
-        begin = a 
-        end = b 
+        begin = a
+        end = b
     else:
-        begin = 0 
-        end = a 
+        begin = 0
+        end = a
     if c is not None:
-        skip = c 
+        skip = c
     if c is None:
-        skip = 1 
+        skip = 1
     i = begin
     while (skip > 0 and i < end) or (skip < 0 and i > end):
         yield i
